f7c528980c532acf9428a1ca72852fd57bb8e849
Nem Sophal <158073813+Nemsophal@users.noreply.github.com>
2026-05-16 22:26:51 +0700

    setup structure

---
+++ src/test/java/org/group1/coffeeshopapi/CoffeeShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.group1.coffeeshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
